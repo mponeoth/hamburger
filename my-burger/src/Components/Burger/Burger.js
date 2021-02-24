aef4ae5d871422fd68fd3495e2c73b0609f171cc
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-02-24 20:54:41 +0300

    129 convert from object to array javascript and map

--- my-burger/src/Components/Burger/Burger.js
+++ my-burger/src/Components/Burger/Burger.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import classes from './Burger.css'
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient'
-
+ 
 const Burger = (props) =>{
+    const transformedIngredients = Object.keys(props.ingredients)//we transformed keys when they`re objec to array
+        .map(igKey =>{
+            return  [...Array(props.ingredients[igKey])].map((_, i)=>{
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+        //you know that map executes a funtion on each element in the input array
+        //[...Array(3)] array three will give you an array with three empty spaces basically with three undifined  spaces
 
         return (
-        <div className={classes.Burger}> 
-            <BurgerIngredient type="bread-top" />      
-            <BurgerIngredient type="cheese" /> 
-            <BurgerIngredient type="meat" /> 
-            <BurgerIngredient type="bread-bottom" />      
+                <div className={classes.Burger}> 
+                    <BurgerIngredient type="bread-top" />      
+                    {transformedIngredients}
+                    <BurgerIngredient type="bread-bottom" />      
 
-        </div>
-        );
+                </div>
+             );
 };
 
 
--- my-burger/src/Containers/BurgerBuilders.js
+++ my-burger/src/Containers/BurgerBuilders.js
@@ -4,10 +4,27 @@ import Burger from '../Components/Burger/Burger'
 
 class  BurgerBuilders extends Component{
     
+    // constructor(props){  
+    //     super(props);
+    //     this.state={...}
+    // }    instead of this we can use just state 
+
+    state = {
+        ingredients:{   
+            salad:5,
+            Bacon:1,
+            cheese:2,
+            meat:2,
+
+        }
+    }
+    //we have to keep in mind  we cant use map because its not array  its object 
+
+
     render(){
         return (
             <AAux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Controls Burger</div>
             </AAux>
                
