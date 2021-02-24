1fb0c98a83567fe59dbbf683919406680eddaeb4
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-02-24 16:49:50 +0300

    126 127 128

--- my-burger/src/Containers/BurgerBuilders.js
+++ my-burger/src/Containers/BurgerBuilders.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AAux from '../hoc/AAux'
+import Burger from '../Components/Burger/Burger'
 
-const BurgerBuilders = () => {
-    return (
-       <AAux>
-           <div>Burger</div>
-           <div>Controls Burger</div>
-       </AAux>
-          
-    );
+class  BurgerBuilders extends Component{
+    
+    render(){
+        return (
+            <AAux>
+                <Burger />
+                <div>Controls Burger</div>
+            </AAux>
+               
+         );
+    }
+   
 };
 
 
