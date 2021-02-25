9eb1c111f55fe4dfd29e78a23d93be2476ff0384
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-02-25 18:43:34 +0300

    135 display and update burger price

--- my-burger/src/Components/Burger/BuildControls/BuildControls.js
+++ my-burger/src/Components/Burger/BuildControls/BuildControls.js
@@ -3,15 +3,19 @@ import classes from './BuildControls.css'
 import BuildControl from  './BuildControl/BuildControl'
 
 const controls = [      //we just created a constant not state not a property  
-    {label:'Salad', type:'salad'},//you should have the same casing and so on as here in type property
-    {label:'Cheese', type:'cheese'},
-    {label:'Bacon', type:'Bacon'},
-    {label:'Meat', type:'meat'},
+    {label:'Salad',type:'salad'},//you should have the same casing and so on as here in type property
+    {label:'Cheese',type:'cheese'},
+    {label:'Bacon',type:'Bacon'},
+    {label:'Meat',type:'meat'},
 ];  
 
 const buildControls = (props) => (
 
+
          <div className={classes.BuildControls}>
+             <p>Current Price:<strong>{props.price.toFixed(2)}</strong></p>
+
+
              {controls.map(ctrl =>(
                  <BuildControl 
                  key={ctrl.label} 
