188da648851d3fd4b32d17f074a7bb5c2ce7731e
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-02-28 13:46:06 +0300

    145 Adding reusable Navigation items

--- my-burger/src/Components/Navigation/Toolbar/Toolbar.js
+++ my-burger/src/Components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import classes from './Toobar.css'
 import Logo from '../../logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
         
