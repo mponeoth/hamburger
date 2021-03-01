0012355ff1efcf08d0ee46839a485ab3c7d9d3c2
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-01 20:33:37 +0300

    151 adding hamburger icon --- instead menu

--- my-burger/src/Components/Navigation/SideDrawer/Drawertoggle/Drawertoggle.js
+++ my-burger/src/Components/Navigation/SideDrawer/Drawertoggle/Drawertoggle.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import classes from './Drawertoggle.css'
+
 const Drawertoggle = (props) =>(
-        <div onClick={props.clicked}>Menu</div>
+        <div onClick={props.clicked} className={classes.DrawerToggle} >
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
 
     )
 
