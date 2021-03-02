e33dab9bb1b248327a877ecdd89ab885630c875c
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-02 18:11:26 +0300

    162 Creating a Http Request to get data

--- my-burger/src/containers/Blog/Blog.js
+++ my-burger/src/containers/Blog/Blog.js
@@ -1,7 +1,5 @@
-//Axios is a third-party javascript library which you can add to any javascript project its not connected to React at all 
-//but of course it fits nicely into React because its Javascript this is not something strongly connect to React 
-//npm i axios --save
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -9,6 +7,19 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    componentDidMount(){
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response =>{
+                console.log(response);
+            })
+            //we sent the request to And for dummy placeholder services
+    }
+                // now we can use it here  in componentDidMount axios offers a GET method for sending you guessed it 
+
+                //* //a list of posts we fetched from a server to  be a precise */
+                //* componentDidMount is a great place for causing side effects but not for updating state since it triggers a re-renders */
+                //* componentDidMount is the best place to send a HTTP request  */
     render () {
         return (
             <div>
@@ -16,6 +27,7 @@ class Blog extends Component {
                     <Post />
                     <Post />
                     <Post />
+             
                 </section>
                 <section>
                     <FullPost />
@@ -29,3 +41,7 @@ class Blog extends Component {
 }
 
 export default Blog;
+
+//Axios is a third-party javascript library which you can add to any javascript project its not connected to React at all 
+//but of course it fits nicely into React because its Javascript this is not something strongly connect to React 
+//npm i axios --save
