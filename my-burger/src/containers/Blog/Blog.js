e11f8d66c7c61f34a62e23969652174e3253501d
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-03 12:20:35 +0300

    163 Rendering Fetched Data to the screen

--- my-burger/src/containers/Blog/Blog.js
+++ my-burger/src/containers/Blog/Blog.js
@@ -8,26 +8,29 @@ import './Blog.css';
 
 class Blog extends Component {
 
+        state = {
+            Posts:[]
+        }
+
     componentDidMount(){
             axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response =>{
-                console.log(response);
-            })
-            //we sent the request to And for dummy placeholder services
+                this.setState({Posts:response.data})
+                console.log(response)//when we use console.log we can see data  with setState we can  get our data
+             })
     }
-                // now we can use it here  in componentDidMount axios offers a GET method for sending you guessed it 
 
-                //* //a list of posts we fetched from a server to  be a precise */
-                //* componentDidMount is a great place for causing side effects but not for updating state since it triggers a re-renders */
-                //* componentDidMount is the best place to send a HTTP request  */
     render () {
+ 
+            const Postsa =this.state.Posts.map(post =>
+                {return <Post key={post.id} title={post.title}/>} )
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-             
+
+                    {Postsa}
+                    
                 </section>
                 <section>
                     <FullPost />
@@ -45,3 +48,11 @@ export default Blog;
 //Axios is a third-party javascript library which you can add to any javascript project its not connected to React at all 
 //but of course it fits nicely into React because its Javascript this is not something strongly connect to React 
 //npm i axios --save
+
+//we sent the request to And for dummy placeholder services
+
+// now we can use it here  in componentDidMount axios offers a GET method for sending you guessed it 
+
+//* //a list of posts we fetched from a server to  be a precise */
+//* componentDidMount is a great place for causing side effects but not for updating state since it triggers a re-renders */
+//* componentDidMount is the best place to send a HTTP request  */
