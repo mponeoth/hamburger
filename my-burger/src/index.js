e17cbecef6a7c1c9de30f0f088b3fbb8088f791a
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-04 19:29:01 +0300

    171 Adding interceptors to Execute Code globally
    we can  find error easily this way

--- my-burger/src/index.js
+++ my-burger/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+
+axios.interceptors.request.use(request =>{
+    // Do something before request is sent
+    console.log(request)
+    //edit request config
+    //it will effect all request sent from anywhere in your app 
+    // Do something with request error
+    return request
+},error =>{
+    console.log(error)
+    return Promise.reject(error)
+})
+// Add a response interceptor
+axios.interceptors.response.use(response =>{
+    console.log(response)
+    return response;   
+},error =>{
+    console.log(error)
+    // Do something with response error
+    return Promise.reject(error)
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
