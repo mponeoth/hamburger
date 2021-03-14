9f22567cadd27a1652a3a2c8cec88d02b48799a9
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-14 15:29:34 +0300

    190 Setting Up the Router Package
    we have just installed Router package and change some file(Fullpost Newpost create Posts), in app we import React router dom

--- my-burger/src/App.js
+++ my-burger/src/App.js
@@ -1,13 +1,17 @@
+//we need to wrap it with a component you import from the react-router-dom package we can do it in both index and appjs  but we will do in app js
 import React, { Component } from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- my-burger/src/containers/Blog/Blog.js
+++ my-burger/src/containers/Blog/Blog.js
@@ -1,10 +1,8 @@
+//npm install --save react-router react-router-dom installed first because we want to use react-router
 import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -65,12 +63,7 @@ class Blog extends Component {
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+              
             </div>
         );
     }
