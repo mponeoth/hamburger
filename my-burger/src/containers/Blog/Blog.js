ee89b707896eb5b0940943f674318cc94826a9ea
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-15 01:29:55 +0300

    196 Using links to Swich Pages
    we use switch pages  for reload page and also we prevent spinning all is in blog

--- my-burger/src/containers/Blog/Blog.js
+++ my-burger/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 //npm install --save react-router react-router-dom installed first because we want to use react-router
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Link } from 'react-router-dom';
 // import axios from 'axios';
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts' 
@@ -15,15 +15,19 @@ class Blog extends Component {
                    <header>
                         <nav>
                             <ul>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/new-post">New-post</a></li>
-                               
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to={{
+                                    pathname:'/new-post',
+                                    hash:'#submit',
+                                    searc:'?quick-submit=true'
+                                }}>New-post</Link></li>
+        {/* when loading same page again its still react rendering the javascript to dom but no spinning */}
                             </ul>
     
                         </nav>
                    </header>
-                    <Route path="/" exact component={Posts} />
-                    <Route path="/new-post"  component={NewPost} />
+                    {/* <Route path="/" exact component={Posts} />  //to show this i comment in github
+                    // <Route path="/new-post"  component={NewPost} /> */} //to show this i comment in github
                     {/* here that is the default case which will use a lot */}
 {/* here simply to what we import from the posts component what we previously directly used in jsx  we simply parsed this curly braces like that   */}
                 </div>
