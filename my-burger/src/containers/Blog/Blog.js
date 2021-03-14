ec96df3459e14113d486680a137b9374cc09e707
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-15 00:34:16 +0300

    193 Setting Up and Rendering Routes

--- my-burger/src/containers/Blog/Blog.js
+++ my-burger/src/containers/Blog/Blog.js
@@ -1,10 +1,9 @@
 //npm install --save react-router react-router-dom installed first because we want to use react-router
 import React, { Component } from 'react';
+import { Route } from 'react-router';
 // import axios from 'axios';
-
-import './Blog.css';
-import Posts from './Posts/Posts';
-
+ import './Blog.css';
+ 
 class Blog extends Component {
    
 
@@ -21,8 +20,11 @@ class Blog extends Component {
     
                         </nav>
                    </header>
-                    <Posts />
-                  
+                    <Route path="/" exact render={()=> <h1>Home</h1>} />
+                    <Route path="/" exact render={()=> <h1>Home2</h1>} />
+  {/* without exact(o sayfaya ozgu) exact means only for this (/) page it can be shown if we dont exact we see that link all pages   */}
+ {/* we want to load posts component we already turned on routing in the app.js by wrapping everything with the browser router  */}
+{/* in the blog component where i want to load my different pages i need to import something else from       */}
                 </div>
             );
         }
