c0f0d64a970ec31dc881719c56d415bdc2d7c14d
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-20 10:13:25 +0300

    206 Navigating Programmatically

--- my-burger/src/containers/Blog/Posts/Posts.js
+++ my-burger/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from '../../../axios';
-import { Link } from 'react-router-dom'
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 
@@ -32,8 +31,9 @@ class Posts extends Component{
             });
     }
     
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+    postSelectedHandler = (id) => {//this is the second way navigation instead of (Link)
+        // this.props.history.push({pathname:'/' + id}) both will work
+        this.props.history.push('/' + id)
     }
 
     render () {
@@ -41,15 +41,17 @@ class Posts extends Component{
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                <Link to={'/' + post.id} key={post.id} >
+               // <Link to={'/' + post.id}  >
                         <Post 
+                        key={post.id}
                         title={post.title} 
                         author={post.author}
                         //{...this.props} this is another way we can information about react-router information like match history location staticContext
                         clicked={() => this.postSelectedHandler(post.id)}/>
-                </Link>);
+                //</Link>
+                );
     //when clicking on this i actually now want to load the full post for this we of course also need to determine where this full post should be shown  
-            });
+            })
         }
        
             return(
