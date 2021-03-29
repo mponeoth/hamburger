3bb24a22e8cf4c04d802ec507c491bc082914d14
muhammetkuruoglu <58957165+muhammetkuruoglu@users.noreply.github.com>
2021-03-29 23:10:01 +0100

    216 Lazy Loading with React Suspense (16.6) Chunks
    for bigger application we can use lazy loading by the way in App folder I updated 21s number from down  app.js

--- my-burger/src/App.js
+++ my-burger/src/App.js
@@ -1,19 +1,58 @@
-//we need to wrap it with a component you import from the react-router-dom package we can do it in both index and appjs  but we will do in app js
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Blog from './containers/Blog/Blog';
+import React, { Component,Suspense } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+//react lazy is a method that was added with  "react": "^16.6.0", that allows us to load components asynchronously which means they are only loading the code behind them is only loaded when they are being rendered 
+//this is for example useful when having routing in your application because only when a user visits a certain routes that component will be required and react lazy allows you to defer(ertelemek) the rendering and the loading 
+//of the code of that component until it is required this is a neat(duzenli)  
+//if you have very simple components using suspends might actually be overkill and could even slow down your application for bigger application very useful
+import User from './containers/User';
+import Welcome from './containers/Welcome';
+const Posts = React.lazy(()=> import ('./containers/Posts'));
 
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
-    );
+    state ={
+      showPost:false
+    }
+    toggleHandler = () =>{
+      this.setState( prevState =>{
+        return {showPost :!prevState.showPost} 
+      })
   }
+
+  render() {
+   
+    return (
+      <React.Fragment>
+      <button onClick={this.toggleHandler}>Toggle</button>
+      {this.state.showPost ? (
+        <Suspense fallback={<div>loading...</div>}>
+            <Posts />
+          </Suspense> )  :  (
+          <User />
+        )}
+      </React.Fragment>
+    
+            
+    
+     
+    
+    //   <BrowserRouter>
+    //     <React.Fragment>
+    //       <nav>
+    //         <NavLink to="/user">User Page</NavLink> |&nbsp;
+    //         <NavLink to="/posts">Posts Page</NavLink>
+    //       </nav>
+    //       <Route path="/" component={Welcome} exact />
+    //       <Route path="/user" component={User} />
+    //       <Route path="/posts" render={()=>
+    //       <Suspense fallback={<div>loading...</div>}>
+    //         <Posts />
+    //       </Suspense>} />
+    //     </React.Fragment>
+    //   </BrowserRouter>
+    // );
+    )
+          }
 }
 
 export default App;
