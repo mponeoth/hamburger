6d9b12550ab27209b5e6ddd3f69d245424eeafd4
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-04-15 16:41:56 +0100

    11 Adding routing to our burger project
    i changed App and index

--- my-burger/src/App.js
+++ my-burger/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import { Route,Switch } from 'react-router-dom'
+
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/BurgerBuilder/checkout/Checkout'
 
 class App extends Component {
-   
 
   render () {
     return (
       <div>
-        <Layout>
-          <BurgerBuilder />
-          <Checkout />
+        <Layout>  
+          <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/" exact component={BurgerBuilder} /> 
+       {/* biz exact yazdigimiz icin burgerbuilder ustte olmasina gerek yok cunku biz exact yazdik nolmade checout un ustune yazmamiz gerekirdi  */}
+          </Switch>
         </Layout>
       </div>
-    );
+    );  
   }
 }
 
