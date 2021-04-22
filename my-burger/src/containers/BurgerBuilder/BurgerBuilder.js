c468f236077ab00b2a0a9bb82f353387a28f2f78
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-04-23 00:07:04 +0100

    11 8 Order Submission & Passing Data Between Pages

--- my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,7 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props)
-        axios.get( 'https://react-my-burger-b9d53-default-rtdb.firebaseio.com/ingredients.json' )
+        axios.get( '/ingredients.json' )
             .then( response => {
                 this.setState( { ingredients: response.data } );
             } )
@@ -92,36 +92,14 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        // this.setState( { loading: true } );
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Max Schwarzmüller',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41351',
-        //             country: 'Germany'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post( '/orders.json', order )
-        //     .then( response => {
-        //         this.setState( { loading: false, purchasing: false } );
-        //     } )
-        //     .catch( error => {
-        //         this.setState( { loading: false, purchasing: false } );
-        //     } ); 
-        //because i dont want to store them in firebase i went to go to checkout instead  
+
 
         //1)way this.props.history.push('/checkout')
         const queryParams = [];
         for (let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))   
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname:'/checkout',
--- my-burger/src/containers/BurgerBuilder/checkout/Checkout.js
+++ my-burger/src/containers/BurgerBuilder/checkout/Checkout.js
@@ -5,21 +5,24 @@ import ContactData from './ContactData/ContactData'
 
 class Checkout  extends Component {
     state ={
-        ingredients:{
-            salad:1,
-            bacon:1,
-            cheese:1,
-            meat:1
-        }
+        ingredients:null,
+        price:0
     }
-    componentDidMount() {
+    componentWillMount () {
+        console.log(this.props)
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()){
             //['salad' , '1']
-            ingredients[param[0]] = +param[1];
-        }
-        this.setState({ingredients: ingredients});
+            if(param[0] === 'price') {
+                price=param[1]
+            }
+            else{
+                ingredients[param[0]] = +param[1]
+            }
+         }
+        this.setState({ingredients: ingredients, totalPrice:price});
     }
 
     checkoutCancelHandler = () =>{
@@ -42,7 +45,7 @@ class Checkout  extends Component {
                 checkoutContinue={this.checkoutContinueHandler} />
                 <Route 
                 path={this.props.match.path + '/contact-data'}
-                component={ContactData} />
+                render={(props)=> (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}/>
             </div>
         )
     }
