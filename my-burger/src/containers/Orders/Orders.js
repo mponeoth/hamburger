ac35b84c339295be031c28b78c24b216ea5ce09f
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-04-24 13:52:05 +0100

    11 11 fetching orders

--- my-burger/src/containers/Orders/Orders.js
+++ my-burger/src/containers/Orders/Orders.js
@@ -1,8 +1,35 @@
 import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
+import widthErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component{
+    
+    state ={
+        orders:[],
+        loading:true
+    }
+    
+
+    componentDidMount(){
+        axios.get('/orders.json')
+            .then(res =>{
+                console.log(res.data)//see my data in console i wrote 
+                const fetchedOrders = []
+                for(let key in res.data){
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id:key
+                    })
+                }
+                this.setState({loading:false, orders:fetchedOrders})
+            })
+            .catch(err =>{
+                this.setState({loading:false})
+            })
+    }
+
     render(){
         return(
             <div>
@@ -13,4 +40,4 @@ class Orders extends Component{
     }
 }
 //i need to output of course should be fetched from bachend 
-export default Orders
+export default widthErrorHandler(Orders,axios)
